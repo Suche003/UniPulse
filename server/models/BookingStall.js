f82d8bfae2dc1d1f186f5add394fe94292b2012a
Malithi <malithipabodha08@gmail.com>
2026-04-04 15:06:51 +0530

    Created Booking Stall Model Class

---
+++ server/models/BookingStall.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const bookingStallSchema = new mongoose.Schema(
+  {
+    bookingId: { type: String, required: true, unique: true }, 
+    eventid: { type: String, required: true },
+    title: { type: String, required: true },
+    stallId: { type: String, required: true },
+    category: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    type: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected", "booked"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const BookingStall = mongoose.model("BookingStall", bookingStallSchema);
+export default BookingStall;
